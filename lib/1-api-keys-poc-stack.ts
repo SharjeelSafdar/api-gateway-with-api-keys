4ce59015647d3cf610b3576becaf9fc9c9b24238
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2022-01-16 14:24:42 +0500

    Initialized a new CDK app

---
+++ lib/1-api-keys-poc-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class 1ApiKeysPocStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, '1ApiKeysPocQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
