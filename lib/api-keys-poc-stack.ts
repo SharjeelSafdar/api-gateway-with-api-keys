431e9a993a47f28d57de7f0ad62e3ce085586962
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2022-01-17 14:40:20 +0500

    Added a Cognito Userpool authorizer for access control

--- lib/api-keys-poc-stack.ts
+++ lib/api-keys-poc-stack.ts
@@ -1,29 +1,41 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 import * as apigw from "@aws-cdk/aws-apigatewayv2-alpha";
 import * as apigwInteg from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as apigwAuth from "@aws-cdk/aws-apigatewayv2-authorizers-alpha";
 
 export class ApiKeysPocStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const api = new apigw.HttpApi(this, "MyApi", {
-      apiName: "test-api-for-api-keys",
-    });
-
     const myLambda = new lambda.Function(this, "MyLambda", {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromInline(`exports.handler = async () => {
-        return {
-          statusCode: 200,
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            message: "Hello, World!",
-          }),
-        };
-      }`),
+      code: lambda.Code.fromAsset("lambda"),
       handler: "index.handler",
+    });
+
+    const userpool = new cognito.UserPool(this, "myUserpool");
+    userpool.addClient("myUserpoolClient", {
+      authFlows: {
+        userPassword: true,
+      },
+    });
+    userpool.addDomain("myUserpoolDomain", {
+      cognitoDomain: {
+        domainPrefix: "api-keys-poc",
+      },
+    });
+
+    const defaultAuthorizer = new apigwAuth.HttpUserPoolAuthorizer(
+      "myAuthorizer",
+      userpool
+    );
+
+    const api = new apigw.HttpApi(this, "MyApi", {
+      apiName: "test-api-for-api-keys",
+      defaultAuthorizer,
     });
 
     api.addRoutes({
